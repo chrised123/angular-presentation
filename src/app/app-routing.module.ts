b4e537bc11115748a2bf017963ab6a6749fc7654
chrised132 <sanjithchendrika@gmail.com>
2020-02-11 15:02:13 +0800

    npm save hmr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { AuthGuardService } from './auth/login.authguard';
 
 
 const routes: Routes = [
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
-  data: { showHeader: false, showFooter: false } },
+  { path: 'login', loadChildren: () => import('./login/.module').then(m => m.LoginModule),
+  data: { showHeader: false, showFooter: false } },login
   { path: 'app', loadChildren: () => import('./core/core.module').then(m => m.CoreModule), canActivate: [AuthGuardService]},
   { path: '', redirectTo: 'login' , pathMatch: 'full'}
 ];
